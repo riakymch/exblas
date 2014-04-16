013696857deacf7e3892176d212746a35e3f915d
riakymch <roman.iakymchuk@sorbonne-universite.fr>
2014-04-16 15:49:42 +0200

    DDOT: the program runs, however there is still a bug in the GPU part

---
+++ DDOT/src/DDOT.Launcher.cpp
@@ -0,0 +1,197 @@
+/*
+ * Copyright 1993-2010 NVIDIA Corporation.  All rights reserved.
+ *
+ * Please refer to the NVIDIA end user license agreement (EULA) associated
+ * with this source code for terms and conditions that govern your use of
+ * this software. Any use, reproduction, disclosure, or distribution of
+ * this software and related documentation outside the terms of the EULA
+ * is strictly prohibited.
+ *
+ */
+
+#include "common.hpp"
+
+////////////////////////////////////////////////////////////////////////////////
+// OpenCL launcher for bitonic sort kernel
+////////////////////////////////////////////////////////////////////////////////
+#define REDUCTION_KERNEL          "DDOT"
+#define REDUCTION_COMPLETE_KERNEL "DDOTComplete"
+
+static size_t szKernelLength;		       // Byte size of kernel code
+static char* cSources = NULL;                  // Buffer to hold source for compilation
+
+static cl_program       cpProgram;             //OpenCL Reduction program
+static cl_kernel        ckKernel, ckComplete;
+static cl_command_queue cqDefaultCommandQue;   //Default command queue for Reduction
+static cl_mem           d_PartialSuperaccs;
+
+static const uint  PARTIAL_SUPERACCS_COUNT = 2048;
+static const uint  WARP_SIZE                  = 32;
+static const uint  WORKGROUP_SIZE             = 256;
+static const uint  MERGE_WORKGROUP_SIZE       = 256;
+static const uint  VECTOR_NUMBER              = 1;
+
+#ifdef AMD
+static char  compileOptions[256] = "-DWARP_SIZE=16 -DWORKGROUP_SIZE=256 -DMERGE_WORKGROUP_SIZE=256";
+#else
+static char  compileOptions[256] = "-DWARP_SIZE=16 -DWORKGROUP_SIZE=256 -DMERGE_WORKGROUP_SIZE=256 -cl-mad-enable -cl-fast-relaxed-math";
+//static char  compileOptions[256] = "-DWARP_SIZE=16 -DWORKGROUP_SIZE=256 -DMERGE_WORKGROUP_SIZE=256 -cl-nv-verbose";
+#endif
+
+
+extern "C" cl_int initDDOT(
+    cl_context cxGPUContext, 
+    cl_command_queue cqParamCommandQue, 
+    cl_device_id cdDevice,
+    const char* program_file
+){
+    cl_int ciErrNum;
+    size_t kernelLength;
+
+    // Read the OpenCL kernel in from source file
+    FILE *program_handle;
+    printf("Load the program sources (%s)...\n", program_file);
+    program_handle = fopen(program_file, "r");
+    if (!program_handle) {
+        fprintf(stderr, "Failed to load kernel.\n");
+        exit(1);
+    }
+    fseek(program_handle, 0, SEEK_END);
+    szKernelLength = ftell(program_handle);
+    rewind(program_handle);
+    cSources = (char *) malloc(szKernelLength + 1);
+    cSources[szKernelLength] = '\0';
+    ciErrNum = fread(cSources, sizeof(char), szKernelLength, program_handle);
+    fclose(program_handle);
+
+    printf("clCreateProgramWithSource...\n"); 
+        cpProgram = clCreateProgramWithSource(cxGPUContext, 1, (const char **)&cSources, &kernelLength, &ciErrNum);
+        if (ciErrNum != CL_SUCCESS) {
+            printf("Error in clCreateProgramWithSource, Line %u in file %s !!!\n\n", __LINE__, __FILE__);
+            return EXIT_FAILURE;
+        }
+
+    printf("...building Reduction program\n");
+        ciErrNum = clBuildProgram(cpProgram, 0, NULL, compileOptions, NULL, NULL);
+        if (ciErrNum != CL_SUCCESS) {
+            //printf("Error in clBuildProgram, Line %u in file %s !!!\n\n", __LINE__, __FILE__);
+
+            // Determine the reason for the error
+            char buildLog[4096];
+            clGetProgramBuildInfo(cpProgram, cdDevice, CL_PROGRAM_BUILD_LOG, sizeof(buildLog), &buildLog, NULL);
+            printf("%s\n", buildLog);
+
+            return EXIT_FAILURE;
+        }
+
+    printf("...creating Reduction kernels:\n");
+        ckKernel = clCreateKernel(cpProgram, REDUCTION_KERNEL, &ciErrNum);
+        if (ciErrNum != CL_SUCCESS) {
+            printf("Error in clCreateKernel: Reduction, Line %u in file %s !!!\n\n", __LINE__, __FILE__);
+            return EXIT_FAILURE;
+        }
+        ckComplete = clCreateKernel(cpProgram, REDUCTION_COMPLETE_KERNEL, &ciErrNum);
+        if (ciErrNum != CL_SUCCESS) {
+            printf("Error in clCreateKernel: Reduction_Complete, Line %u in file %s !!!\n\n", __LINE__, __FILE__);
+            return EXIT_FAILURE;
+        }
+    printf("...allocating internal buffer\n");
+        d_PartialSuperaccs = clCreateBuffer(cxGPUContext, CL_MEM_READ_WRITE, PARTIAL_SUPERACCS_COUNT * sizeof(cl_double), NULL, &ciErrNum);
+        if (ciErrNum != CL_SUCCESS) {
+            printf("Error in clCreateBuffer, Line %u in file %s !!!\n\n", __LINE__, __FILE__);
+            return EXIT_FAILURE;
+        }
+
+    //Save default command queue
+    cqDefaultCommandQue = cqParamCommandQue;
+
+    //Discard temp storage
+    free(cSources);
+
+    return EXIT_SUCCESS;
+}
+
+extern "C" void closeDDOT(void){
+    cl_int ciErrNum;
+
+    ciErrNum = clReleaseMemObject(d_PartialSuperaccs);
+    ciErrNum |= clReleaseKernel(ckKernel);
+    ciErrNum |= clReleaseKernel(ckComplete);
+    ciErrNum |= clReleaseProgram(cpProgram);
+    if (ciErrNum != CL_SUCCESS) {
+        printf("Error in closeReduction(), Line %u in file %s !!!\n\n", __LINE__, __FILE__);
+    }
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// OpenCL launchers for Reduction / mergeReduction kernels
+////////////////////////////////////////////////////////////////////////////////
+//Round a / b to nearest higher integer value
+inline uint iDivUp(uint a, uint b){
+    return (a % b != 0) ? (a / b + 1) : (a / b);
+}
+
+//Snap a to nearest lower multiple of b
+inline uint iSnapDown(uint a, uint b){
+    return a - a % b;
+}
+
+extern "C" size_t DDOT(
+    cl_command_queue cqCommandQueue,
+    cl_mem d_Superacc,
+    cl_mem d_a,
+    cl_mem d_b,
+    uint NbElements,
+    cl_int *ciErrNumRes
+){
+    cl_int ciErrNum;
+    size_t NbThreadsPerWorkGroup, TotalNbThreads;
+
+    if(!cqCommandQueue)
+        cqCommandQueue = cqDefaultCommandQue;
+
+    {
+        NbThreadsPerWorkGroup  = WORKGROUP_SIZE;
+        TotalNbThreads = PARTIAL_SUPERACCS_COUNT * NbThreadsPerWorkGroup;
+        NbElements = NbElements / VECTOR_NUMBER;
+
+        ciErrNum  = clSetKernelArg(ckKernel, 0, sizeof(cl_mem),  (void *)&d_PartialSuperaccs);
+        ciErrNum |= clSetKernelArg(ckKernel, 1, sizeof(cl_mem),  (void *)&d_a);
+        ciErrNum |= clSetKernelArg(ckKernel, 2, sizeof(cl_mem),  (void *)&d_b);
+        ciErrNum |= clSetKernelArg(ckKernel, 3, sizeof(cl_uint), (void *)&NbElements);
+        if (ciErrNum != CL_SUCCESS) {
+            printf("Error in clSetKernelArg, Line %u in file %s !!!\n\n", __LINE__, __FILE__);
+	    *ciErrNumRes = EXIT_FAILURE;
+            return 0;
+        }
+
+        ciErrNum = clEnqueueNDRangeKernel(cqCommandQueue, ckKernel, 1, NULL, &TotalNbThreads, &NbThreadsPerWorkGroup, 0, NULL, NULL);
+        if (ciErrNum != CL_SUCCESS) {
+            printf("Error in clEnqueueNDRangeKernel, Line %u in file %s !!!\n\n", __LINE__, __FILE__);
+	    *ciErrNumRes = EXIT_FAILURE;
+            return 0;
+        }
+    }
+    {
+        NbThreadsPerWorkGroup = MERGE_WORKGROUP_SIZE;
+        TotalNbThreads = NbThreadsPerWorkGroup;
+
+        ciErrNum  = clSetKernelArg(ckComplete, 0, sizeof(cl_mem),  (void *)&d_Superacc);
+        ciErrNum |= clSetKernelArg(ckComplete, 1, sizeof(cl_mem),  (void *)&d_PartialSuperaccs);
+        ciErrNum |= clSetKernelArg(ckComplete, 2, sizeof(cl_uint), (void *)&PARTIAL_SUPERACCS_COUNT);
+        if (ciErrNum != CL_SUCCESS) {
+            printf("Error in clSetKernelArg, Line %u in file %s !!!\n\n", __LINE__, __FILE__);
+	    *ciErrNumRes = EXIT_FAILURE;
+            return 0;
+        }
+
+        ciErrNum = clEnqueueNDRangeKernel(cqCommandQueue, ckComplete, 1, NULL, &TotalNbThreads, &NbThreadsPerWorkGroup, 0, NULL, NULL);
+        if (ciErrNum != CL_SUCCESS) {
+            printf("Error in clEnqueueNDRangeKernel, Line %u in file %s !!!\n\n", __LINE__, __FILE__);
+	    *ciErrNumRes = EXIT_FAILURE;
+            return 0;
+        }
+    }
+
+    return WORKGROUP_SIZE;
+}
---
+++ DDOT/src/DDOT.cl
@@ -0,0 +1,83 @@
+
+#pragma OPENCL EXTENSION cl_khr_fp64                   : enable  // For double precision numbers
+#pragma OPENCL EXTENSION cl_khr_byte_addressable_store : enable
+
+//Data type used for input data fetches
+typedef double data_t;
+
+
+////////////////////////////////////////////////////////////////////////////////
+// Main computation pass: compute partial reductions
+////////////////////////////////////////////////////////////////////////////////
+__kernel __attribute__((reqd_work_group_size(WORKGROUP_SIZE, 1, 1)))
+void DDOT(
+    __global data_t *d_PartialSuperaccs,
+    __global data_t *d_a,
+    __global data_t *d_b,
+    const uint NbElements
+){
+    __local data_t l_sa[WORKGROUP_SIZE] __attribute__((aligned(8)));
+    uint lid = get_local_id(0);
+
+    // Each work-item accumulates as many elements as necessary into local variable "sum"
+    data_t sum = 0.0;
+    for(uint gid = get_global_id(0); gid < NbElements; gid += get_global_size(0)){
+	fma(d_a[gid], d_b[gid], sum);
+    }
+    l_sa[lid] = sum; 
+
+    // Perform parallel reduction to add each work-item's 
+    // partial summation together
+    for (uint stride = get_local_size(0) / 2; stride > 0; stride /= 2) { 
+        // Synchronize to make sure each work-item is done updating 
+        // shared memory; this is necessary because work-items read 
+        // results written by other work-items during each parallel 
+        // reduction step 
+        barrier(CLK_LOCAL_MEM_FENCE); 
+
+        // Only the first work-items in the work-group add elements together 
+        if (get_local_id(0) < stride) { 
+            // Add two elements from the l_sa array 
+            // and store the result in l_sa[index] 
+            l_sa[lid] += l_sa[lid + stride]; 
+        } 
+    } 
+
+    // Write the result of the reduction to global memory 
+    if (lid == 0)
+        d_PartialSuperaccs[get_group_id(0)] = l_sa[0];
+
+    // Synchronize to make sure the first work-item is done reading partialSummation 
+    barrier(CLK_LOCAL_MEM_FENCE);
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// Merging
+////////////////////////////////////////////////////////////////////////////////
+__kernel __attribute__((reqd_work_group_size(MERGE_WORKGROUP_SIZE, 1, 1)))
+void DDOTComplete(
+    __global data_t *d_Superacc,
+    __global data_t *d_PartialSuperaccs,
+    uint NbElements
+){
+    __local data_t l_Data[MERGE_WORKGROUP_SIZE];
+    uint lid = get_local_id(0);
+    uint gid = get_group_id(0);
+
+    data_t sum = 0.0;
+    for(uint i = lid; i < NbElements; i += MERGE_WORKGROUP_SIZE)
+        sum += d_PartialSuperaccs[gid * MERGE_WORKGROUP_SIZE + i];
+    l_Data[lid] = sum;
+
+    for(uint stride = MERGE_WORKGROUP_SIZE / 2; stride > 0; stride /= 2){
+        barrier(CLK_LOCAL_MEM_FENCE);
+        if(lid < stride)
+            l_Data[lid] += l_Data[lid + stride];
+    }
+    
+    if(lid == 0)
+        d_Superacc[gid] = l_Data[0];
+
+    barrier(CLK_LOCAL_MEM_FENCE);
+}
+
