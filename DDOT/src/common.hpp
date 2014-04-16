bcdf5582cd9a62143c2edbc87b23c5e509781c0f
riakymch <roman.iakymchuk@sorbonne-universite.fr>
2014-04-16 13:05:14 +0200

    Working on exact dot product

---
+++ DDOT/src/common.hpp
@@ -0,0 +1,174 @@
+/*
+ * Copyright 1993-2010 NVIDIA Corporation.  All rights reserved.
+ *
+ * Please refer to the NVIDIA end user license agreement (EULA) associated
+ * with this source code for terms and conditions that govern your use of
+ * this software. Any use, reproduction, disclosure, or distribution of
+ * this software and related documentation outside the terms of the EULA
+ * is strictly prohibited.
+ *
+ */
+
+#ifndef COMMON_H
+#define COMMON_H
+
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <string.h>
+
+#include <gmp.h>
+#include <mpfr.h>
+
+#include "DDOT.hpp"
+#include "Superaccumulator.hpp"
+
+// All OpenCL headers
+#if defined (__APPLE__) || defined(MACOSX)
+    #include <OpenCL/opencl.h>
+#else
+    #include <CL/opencl.h>
+#endif
+
+////////////////////////////////////////////////////////////////////////////////
+// Common definitions
+////////////////////////////////////////////////////////////////////////////////
+#define BIN_COUNT 39U
+
+
+#define E_BITS 1023
+#define F_BITS (1023 + 52)
+
+typedef cl_long bintype;
+
+
+////////////////////////////////////////////////////////////////////////////////
+// Common functions
+////////////////////////////////////////////////////////////////////////////////
+cl_platform_id GetOCLPlatform(
+    char name[]
+);
+
+cl_device_id GetOCLDevice(
+    cl_platform_id pPlatform,
+    char name[]
+);
+
+void initVectorWithRandomData(
+    double *data,
+    int size
+);
+
+void init_fpuniform(
+    double *array,
+    int size,
+    int range,
+    int emax
+);
+
+double min(
+    double arr[],
+    int size
+);
+
+////////////////////////////////////////////////////////////////////////////////
+// GPU DDOT product related functions
+////////////////////////////////////////////////////////////////////////////////
+extern "C" cl_int initDDOT(
+    cl_context cxGPUContext,
+    cl_command_queue cqParamCommandQue,
+    cl_device_id cdDevice,
+    const char* program_file
+);
+
+extern "C" cl_int initDDOTAMD(
+    cl_context cxGPUContext,
+    cl_command_queue cqParamCommandQue,
+    cl_device_id cdDevice,
+    const char* program_file
+);
+
+extern "C" cl_int initDDOTNVIDIA(
+    cl_context cxGPUContext,
+    cl_command_queue cqParamCommandQue,
+    cl_device_id cdDevice,
+    const char* program_file
+);
+
+extern "C" cl_int initDDOTRepro(
+    cl_context cxGPUContext,
+    cl_command_queue cqParamCommandQue,
+    cl_device_id cdDevice,
+    const char* program_file,
+    const uint NbFPE,
+    const uint width,
+    const uint height
+);
+
+extern "C" void closeDDOT(
+    void
+);
+
+extern "C" void closeDDOTAMD(
+    void
+);
+
+extern "C" void closeDDOTNVIDIA(
+    void
+);
+
+extern "C" void closeDDOTRepro(
+    void
+);
+
+extern "C" size_t DDOT(
+    cl_command_queue cqCommandQueue,
+    cl_mem d_res,
+    const cl_mem d_a,
+    const cl_mem d_b,
+    cl_int *ciErrNum
+);
+
+extern "C" size_t DDOTAMD(
+    cl_command_queue cqCommandQueue,
+    cl_mem d_res,
+    const cl_mem d_a,
+    const cl_mem d_b,
+    cl_int *ciErrNum
+);
+
+extern "C" size_t DDOTNVIDIA(
+    cl_command_queue cqCommandQueue,
+    cl_mem d_res,
+    const cl_mem d_a,
+    const cl_mem d_b,
+    cl_int *ciErrNum
+);
+
+extern "C" size_t DDOTRepro(
+    cl_command_queue cqCommandQueue,
+    cl_mem d_res,
+    const cl_mem d_a,
+    const cl_mem d_b,
+    cl_int *ciErrNum
+);
+
+////////////////////////////////////////////////////////////////////////////////
+// MPFR and Kahan summation functions
+////////////////////////////////////////////////////////////////////////////////
+extern "C" char *sum_mpfr(
+    double *data,
+    int size);
+
+extern "C" double round_mpfr(
+    double *data,
+    int size
+);
+
+
+////////////////////////////////////////////////////////////////////////////////
+// Executable functions from main.cpp
+////////////////////////////////////////////////////////////////////////////////
+int runDDOT(const char*);
+
+#endif
---
+++ DDOT/src/main.cpp
@@ -0,0 +1,283 @@
+/*
+ * Copyright 1993-2010 NVIDIA Corporation.  All rights reserved.
+ *
+ * Please refer to the NVIDIA end user license agreement (EULA) associated
+ * with this source code for terms and conditions that govern your use of
+ * this software. Any use, reproduction, disclosure, or distribution of
+ * this software and related documentation outside the terms of the EULA
+ * is strictly prohibited.
+ *
+ */
+
+// standard utilities and systems includes
+#include "common.hpp"
+
+#define NUM_ITER  20
+////////////////////////////////////////////////////////////////////////////////
+// Variables used in the program 
+////////////////////////////////////////////////////////////////////////////////
+cl_platform_id    cpPlatform;                   //OpenCL platform
+cl_device_id      cdDevice;                     //OpenCL device list    
+cl_context        cxGPUContext;                 //OpenCL context
+cl_command_queue  cqCommandQueue;               //OpenCL command que
+Matrix            d_a, d_b, d_res;                //OpenCL memory buffer objects
+double            *a, *b, res;
+
+static uint __nbElements = 0;
+static uint __range      = 0;
+static uint __nbfpe      = 0;
+static uint __alg        = 0;
+
+static void __usage(int argc __attribute__((unused)), char **argv) {
+  fprintf(stderr, "Usage: %s [-n number of elements -r range -e nbfpe -a alg (0-mine, 1-amd, 2-nvidia, 3-repro)] \n", argv[0]);
+  printf("       -?, -h:    Display this help and exit\n");
+}
+
+static void __parse_args(int argc, char **argv) {
+  int i;
+
+  for (i = 1; i < argc; i++) {
+    if ((strcmp(argv[i], "-n") == 0)) {
+      __nbElements = atoi(argv[++i]);
+    } if ((strcmp(argv[i], "-r") == 0)) {
+      __range = atoi(argv[++i]);
+    } if ((strcmp(argv[i], "-e") == 0)) {
+      __nbfpe = atoi(argv[++i]);
+    } if ((strcmp(argv[i], "-a") == 0)) {
+      __alg = atoi(argv[++i]);
+    } else if ((strcmp(argv[i], "-h") || strcmp(argv[i], "-?")) == 0) {
+      __usage(argc, argv);
+      exit(-1);
+    } else if (argv[i][0] == '-') {
+      fprintf(stderr, "Unknown option %s\n", argv[i]);
+      __usage(argc, argv);
+      exit(-1);
+    }
+  }
+
+  if (__nbElements <= 0) {
+    __usage(argc, argv);
+    exit(-1);
+  }
+  if (__alg > 3) {
+    __usage(argc, argv);
+    exit(-1);
+  }
+}
+
+int cleanUp (
+    int exitCode
+);
+
+////////////////////////////////////////////////////////////////////////////////
+//Test driver
+////////////////////////////////////////////////////////////////////////////////
+int main(int argc, char **argv)
+{
+    __parse_args(argc, argv);
+    printf("Starting with two vectors with  %i double elements\n\n", __nbElements); 
+
+    if (__alg == 0)
+        runDGEMM("../src/DDOT.cl");
+    if (__alg == 1)
+        //runDGEMM("../src/DGEMM.AMD.cl");
+        runDGEMM("../src/DDOT.AMD.cl");
+    if (__alg == 2)
+        runDGEMM("../src/DDOT.NVIDIA.cl");
+    if (__alg == 3)
+        runDGEMM("../src/DDOT.Repro.cl");
+}
+
+int runDDOT(const char* program_file){
+    cl_int ciErrNum;
+    int PassFailFlag = 1;
+    int nbElements = 0;
+
+    printf("Initializing data...\n");
+        PassFailFlag  = posix_memalign((void **)&a, 64, __nbElements * sizeof(double));
+        PassFailFlag |= posix_memalign((void **)&b, 64, __nbElements * sizeof(double));
+        if (PassFailFlag != 0) {
+            printf("ERROR: could not allocate memory with posix_memalign!\n");
+            exit(1);
+        }
+	// init data
+        int emax = E_BITS - log2(__nbElemetns);// use log in order to stay within [emin, emax]
+        init_fpuniform(a, __nbElements, __range, emax);
+        init_fpuniform(b, __nbElements, __range, emax);
+
+    printf("Initializing OpenCL...\n");
+        char platform_name[64];
+	char device_name[32];
+#ifdef AMD
+        strcpy(platform_name, "AMD Accelerated Parallel Processing");
+	strcpy(device_name, "Tahiti");
+#else
+        strcpy(platform_name, "NVIDIA CUDA");
+        strcpy(device_name, "Tesla K20c");
+#endif
+        //setenv("CUDA_CACHE_DISABLE", "1", 1);
+        cpPlatform = GetOCLPlatform(platform_name);
+        if (cpPlatform == NULL) {
+            printf("ERROR: Failed to find the platform '%s' ...\n", platform_name);
+            return -1;
+        }
+
+        //Get a GPU device
+        cdDevice = GetOCLDevice(cpPlatform, device_name);
+        if (cdDevice == NULL) {
+            printf("ERROR: Failed to find the device '%s' ...\n", device_name);
+            return -1;
+        }
+
+        //Create the context
+        cxGPUContext = clCreateContext(0, 1, &cdDevice, NULL, NULL, &ciErrNum);
+        if (ciErrNum != CL_SUCCESS) {
+            printf("Error in clCreateContext, Line %u in file %s !!!\n\n", __LINE__, __FILE__);
+            cleanUp(EXIT_FAILURE);
+        }
+
+        //Create a command-queue
+        cqCommandQueue = clCreateCommandQueue(cxGPUContext, cdDevice, CL_QUEUE_PROFILING_ENABLE, &ciErrNum);
+        if (ciErrNum != CL_SUCCESS) {
+            printf("Error in clCreateCommandQueue, Line %u in file %s !!!\n\n", __LINE__, __FILE__);
+            cleanUp(EXIT_FAILURE);
+        }
+
+    printf("Allocating OpenCL memory...\n\n");
+	cl_mem d_a;
+	size_t size = __nbElements * sizeof(double);
+	d_A.elements = clCreateBuffer(cxGPUContext, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR, size, a, &ciErrNum);
+        if (ciErrNum != CL_SUCCESS) {
+            printf("Error in clCreateBuffer for d_a, Line %u in file %s !!!\n\n", __LINE__, __FILE__);
+            cleanUp(EXIT_FAILURE);
+        }
+    {
+        printf("Initializing OpenCL DDOT...\n");
+	    if (__alg == 0)
+                ciErrNum = initDDOT(cxGPUContext, cqCommandQueue, cdDevice, program_file);
+            else if (__alg == 1)
+                ciErrNum = initDDOTAMD(cxGPUContext, cqCommandQueue, cdDevice, program_file);
+            else if (__alg == 2)
+                ciErrNum = initDDOTVIDIA(cxGPUContext, cqCommandQueue, cdDevice, program_file);
+            else if (__alg == 3)
+                ciErrNum = initDDOTRepro(cxGPUContext, cqCommandQueue, cdDevice, program_file, __nbfpe);
+            
+            if (ciErrNum != CL_SUCCESS)
+                cleanUp(EXIT_FAILURE);
+
+        printf("Running OpenCL DDOT with %u elements...\n\n", 2.0 * __nbElements);
+            //Just a single launch or a warmup iteration
+            if (__alg == 0)
+                DDOT(NULL, d_res, d_a, d_b, &ciErrNum);
+            else if (__alg == 1)
+                DDOTAMD(NULL, d_res, d_a, d_b, &ciErrNum);
+            else if (__alg == 2)
+                DDOTNVIDIA(NULL, d_res, d_a, d_b, &ciErrNum);
+            else if (__alg == 3)
+                DDOTRepro(NULL, d_res, d_a, d_b, &ciErrNum);
+
+            if (ciErrNum != CL_SUCCESS)
+                cleanUp(EXIT_FAILURE);
+
+#ifdef GPU_PROFILING
+	double gpuTime[NUM_ITER];
+        cl_event startMark, endMark;
+
+        for(uint iter = 0; iter < NUM_ITER; iter++) {
+            ciErrNum = clEnqueueMarker(cqCommandQueue, &startMark);
+            ciErrNum |= clFinish(cqCommandQueue);
+            if (ciErrNum != CL_SUCCESS) {
+                printf("Error in clEnqueueMarker, Line %u in file %s !!!\n\n", __LINE__, __FILE__);
+                cleanUp(EXIT_FAILURE);
+            }
+
+            if (__alg == 0)
+                DDOT(NULL, d_res, d_a, d_b, &ciErrNum);
+            else if (__alg == 1)
+                DDOTAMD(NULL, d_res, d_a, d_b, &ciErrNum);
+            else if (__alg == 2)
+                DDOTNVIDIA(NULL, d_res, d_a, d_b, &ciErrNum);
+            else if (__alg == 3)
+                DDOTRepro(NULL, d_res, d_a, d_b, &ciErrNum);
+
+            ciErrNum  = clEnqueueMarker(cqCommandQueue, &endMark);
+            ciErrNum |= clFinish(cqCommandQueue);
+            if (ciErrNum != CL_SUCCESS) {
+                printf("Error in clEnqueueMarker, Line %u in file %s !!!\n\n", __LINE__, __FILE__);
+                cleanUp(EXIT_FAILURE);
+            }
+
+            //Get OpenCL profiler time
+            cl_ulong startTime = 0, endTime = 0;
+            ciErrNum  = clGetEventProfilingInfo(startMark, CL_PROFILING_COMMAND_END, sizeof(cl_ulong), &startTime, NULL);
+            ciErrNum |= clGetEventProfilingInfo(endMark, CL_PROFILING_COMMAND_END, sizeof(cl_ulong), &endTime, NULL);
+            if (ciErrNum != CL_SUCCESS) {
+                printf("Error in clGetEventProfilingInfo Line %u in file %s !!!\n\n", __LINE__, __FILE__);
+                cleanUp(EXIT_FAILURE);
+            }
+            gpuTime[iter] = 1.0e-9 * ((unsigned long)endTime - (unsigned long)startTime); // / (double)NUM_ITER;
+        }
+
+	double minTime = min(gpuTime, NUM_ITER);
+	double perf = 2.0 * __nbElements * sizeof(double);
+	double throughput = (perf / minTime) * 1e-9;
+	perf = 2.0 * __nbElements;
+	perf = (perf / minTime) * 1e-9;
+        printf("Alg = %u \t Range = %u \t NbElements = %u \t Size = %lu \t Time = %.8f s \t Throughput = %.4f GB/s\n\n", __alg, __range, nbElements, nbElements * sizeof(double), minTime, throughput);
+        printf("Alg = %u \t Range = %u \t NbElements = %u \t Size = %lu \t Time = %.8f s \t Performance = %.4f GFLOPS\n\n", __alg, __range, nbElements, nbElements * sizeof(double), minTime, perf);
+#endif
+
+        printf("Validating DDOT OpenCL results...\n");
+            printf(" ...reading back OpenCL results\n");
+                ciErrNum = clEnqueueReadBuffer(cqCommandQueue, d_res, CL_TRUE, 0, sizeof(double), res, 0, NULL, NULL);
+                if (ciErrNum != CL_SUCCESS) {
+                    printf("Error in clEnqueueReadBuffer Line %u in file %s !!!\n\n", __LINE__, __FILE__);
+                    cleanUp(EXIT_FAILURE);
+                }
+		double res_CPU;
+                dotProduct(res_CPU, a, b, __nbElements);
+		//Compare the GPU to the CPU results
+		printf("Difference = %16.g\n", abs(res - res_CPU));
+		PassFailFlag = abs(res - res_CPU) < 1e-16 ? 1 : 0;
+		free(C_CPU);
+		
+         //Release kernels and program
+         printf("Shutting down...\n\n");
+            if (__alg == 0)
+		closeDDOT();
+            else if (__alg == 1)
+                closeDDOTAMD();
+            else if (__alg == 2)
+                closeDDOTNVIDIA();
+            else if (__alg == 3)
+                closeDDOTRepro();
+    }
+
+    // pass or fail
+    if (PassFailFlag)
+	printf("[DDOT] test results...\tPASSED\n");
+    else
+	printf("[DDOT] test results...\tFAILED\n");
+
+    cleanUp(EXIT_SUCCESS);
+}
+
+int cleanUp (int exitCode) {
+    //Release other OpenCL Objects
+    if(d_a) 
+	clReleaseMemObject(d_a);
+    if(d_b) 
+	clReleaseMemObject(d_b);
+    if(cqCommandQueue) 
+	clReleaseCommandQueue(cqCommandQueue);
+    if(cxGPUContext) 
+	clReleaseContext(cxGPUContext);
+
+    //Release host buffers
+    free(a);
+    free(b);
+    //free(C);
+    
+    return exitCode;
+}
+
