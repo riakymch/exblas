2e1aea870374d0eeb440be20a011758ddaf6f6cd
riakymch <roman.iakymchuk@sorbonne-universite.fr>
2014-06-04 14:14:20 +0200

    Minor changes to the amd version

--- DGEMM/src/DGEMM.Launcher.AMD.cpp
+++ DGEMM/src/DGEMM.Launcher.AMD.cpp
@@ -140,16 +140,16 @@ extern "C" size_t DGEMMAMD(
 
     {
         size_t NbThreadsPerWorkGroup[] = {BLOCK_SIZE, BLOCK_SIZE};
-	size_t widthB = d_B.width / VECTOR_NUMBER;
-	size_t heightA = d_A.height / VECTOR_NUMBER;
-	size_t TotalNbThreads[] = {widthB, heightA};
+	size_t widthC = d_C.width / VECTOR_NUMBER;
+	size_t heightC = d_C.height / VECTOR_NUMBER;
+	size_t TotalNbThreads[] = {widthC, heightC};
 	size_t neededLocalMemory = (BLOCK_SIZE * VECTOR_NUMBER) * (BLOCK_SIZE * VECTOR_NUMBER) * sizeof(cl_double);
 
 	cl_int i = 0;
         ciErrNum  = clSetKernelArg(ckMatrixMul, i++, sizeof(cl_mem),  (void *)&d_C.elements);
         ciErrNum |= clSetKernelArg(ckMatrixMul, i++, sizeof(cl_mem),  (void *)&d_A.elements);
         ciErrNum |= clSetKernelArg(ckMatrixMul, i++, sizeof(cl_mem),  (void *)&d_B.elements);
-        ciErrNum |= clSetKernelArg(ckMatrixMul, i++, sizeof(cl_int),  (void *)&d_A.width);
+        ciErrNum |= clSetKernelArg(ckMatrixMul, i++, sizeof(cl_int),  (void *)&d_C.width);
         ciErrNum |= clSetKernelArg(ckMatrixMul, i++, neededLocalMemory,  NULL);
         if (ciErrNum != CL_SUCCESS) {
             printf("Error in clSetKernelArg, Line %u in file %s !!!\n\n", __LINE__, __FILE__);
