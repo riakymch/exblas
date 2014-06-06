236ed414a15d6bc3b0f47f1c6585794455c0475a
riakymch <roman.iakymchuk@sorbonne-universite.fr>
2014-06-06 17:26:06 +0200

    Tuned the script; minor chanes to the outputs

--- DGEMM/scripts/run.NbElements.VS.Gflops.sh
+++ DGEMM/scripts/run.NbElements.VS.Gflops.sh
@@ -8,49 +8,36 @@ then
   exit 1
 fi
 
-nbMax=1000000000
-step=2
+nMax=4096
+step=256
 exe=./../src/main.out
-file=../results/NbElements.VS.GBs.NVIDIA.2014.04.29.Round.dat
+file=../results/NbElements.VS.Gflops.AMD.2014.06.06.Round.dat
 
 touch $file
 echo -n "" > $file
 
-#naive DDOT
-for ((nbElements=1024; nbElements<=${nbMax}; nbElements*=${step}))
+#DGEMM: 0-mine; 1-amd; 2-nvidia; 3-with superaccs in private memory
+for alg in 0 1 2 3 7 
 do
-    $exe -n $nbElements -r 1 -e 0 -a 0 | tee -a $file
-done
-
-#DDOT using superaccumulator
-for ((nbElements=1024; nbElements<=${nbMax}; nbElements*=${step}))
-do
-    $exe -n $nbElements -r 1 -e 0 -a 1 | tee -a $file
-done
-
-#DDOT using superaccumulator with various FPEs
-for ((range=2; range<=8; range+=1))
-do
-    for ((nbElements=1024; nbElements<=${nbMax}; nbElements*=${step}))
+    for ((n=256; n<=${nMax}; n+=${step}))
     do
-	$exe -n $nbElements -r 1 -e $range -a 2 | tee -a $file
+        $exe -m $n -n $n -k $n -r 1 -e 0 -a $alg | tee -a $file
+    done
+done
+
+#DGEMM with various FPEs: 4-superaccs in private; 6-superaccs in local; 8-superaccs in global
+for alg in 4 6 8 
+    for ((range=2; range<=8; range+=1))
+    do
+        for ((n=256; n<=${nMax}; n+=${step}))
+        do
+            $exe -m $n -n $n -k $n -r 1 -e $range -a $alg | tee -a $file
+        done
     done
 done
 
 #DDOT using superaccumulator with FPE of format 8 and the early-exit
-for ((nbElements=1024; nbElements<=${nbMax}; nbElements*=${step}))
-do
-    $exe -n $nbElements -r 1 -e 8 -a 3 | tee -a $file
-done
-
-#DDOT using superaccumulator with FPE of format 4 and the early-exit
-for ((nbElements=1024; nbElements<=${nbMax}; nbElements*=${step}))
-do
-    $exe -n $nbElements -r 1 -e 4 -a 4 | tee -a $file
-done
-
-#DDOT using superaccumulator with FPE of format 6 and the early-exit
-for ((nbElements=1024; nbElements<=${nbMax}; nbElements*=${step}))
-do
-    $exe -n $nbElements -r 1 -e 6 -a 5 | tee -a $file
-done
+#for ((nbElements=1024; nbElements<=${nbMax}; nbElements*=${step}))
+#do
+#    $exe -n $nbElements -r 1 -e 8 -a 3 | tee -a $file
+#done
