7deeaf86b7184a654c43cd5180e62da01968b1ac
riakymch <roman.iakymchuk@sorbonne-universite.fr>
2014-06-24 12:00:44 +0200

    Working on multi-value variant for private memory case

--- DGEMM/src/main.cpp
+++ DGEMM/src/main.cpp
@@ -31,7 +31,7 @@ static uint __nbfpe      = 0;
 static uint __alg        = 0;
 
 static void __usage(int argc __attribute__((unused)), char **argv) {
-  fprintf(stderr, "Usage: %s [-m nbrows of C -n nbcolumns of C -k nbcolumns of B\n -r range -e nbfpe\n -a alg (0-mine, 1-amd, 2-nvidia, 30-sapr, 31-fpepr, 32-fpee4pr, 33-fpee8pr, 40-salo, 41-fpelo, 50-sagl, 51-fpegl, 52-fpee4gl, 53-fpee8gl, 54-fpeglmulti, 6-new)] \n", argv[0]);
+  fprintf(stderr, "Usage: %s [-m nbrows of C -n nbcolumns of C -k nbcolumns of B\n -r range -e nbfpe\n -a alg (0-mine, 1-amd, 2-nvidia, 30-sapr, 31-fpepr, 32-fpee4pr, 33-fpee8pr, 34-fpeprmulti, 40-salo, 41-fpelo, 50-sagl, 51-fpegl, 52-fpee4gl, 53-fpee8gl, 54-fpeglmulti, 6-new)] \n", argv[0]);
   printf("       -?, -h:    Display this help and exit\n");
 }
 	
@@ -66,9 +66,9 @@ static void __parse_args(int argc, char **argv) {
     exit(-1);
   }
   
-  int algs[]= {0,1,2,30,31,32,33,40,41,50,51,52,53,54,6};
+  int algs[]= {0,1,2,30,31,32,33,34,40,41,50,51,52,53,54,6};
   int is_alg = 0;
-  for (i = 0; i < 14; i++)
+  for (i = 0; i < 15; i++)
     if (algs[i] == __alg) {
       is_alg = 1;
       break;
@@ -105,6 +105,8 @@ int main(int argc, char **argv)
         runDGEMM("../src/DGEMM.NVIDIA.FPE.EX4.Private.cl");
     else if (__alg == 33)
         runDGEMM("../src/DGEMM.NVIDIA.FPE.EX8.Private.cl");
+    else if (__alg == 34)
+        runDGEMM("../src/DGEMM.NVIDIA.FPE.Private.Multi.cl");
     else if (__alg == 40)
         runDGEMM("../src/DGEMM.NVIDIA.Superacc.Local.cl");
     else if (__alg == 41)
@@ -219,7 +221,7 @@ int runDGEMM(const char* program_file){
                 ciErrNum = initDGEMMAMD(cxGPUContext, cqCommandQueue, cdDevice, program_file);
             else if (__alg == 2)
                 ciErrNum = initDGEMMNVIDIA(cxGPUContext, cqCommandQueue, cdDevice, program_file);
-            else if (((__alg >= 30) && (__alg <= 33)) || (__alg == 40) || (__alg == 41))
+            else if (((__alg >= 30) && (__alg <= 34)) || (__alg == 40) || (__alg == 41))
                 ciErrNum = initDGEMMNVIDIAPrivate(cxGPUContext, cqCommandQueue, cdDevice, program_file, __nbfpe, __nbColumnsC, __nbRowsC);
             else if ((__alg >= 50) && (__alg <= 54))
                 ciErrNum = initDGEMMNVIDIAGlobal(cxGPUContext, cqCommandQueue, cdDevice, program_file, __nbfpe, __nbColumnsC, __nbRowsC);
@@ -238,7 +240,7 @@ int runDGEMM(const char* program_file){
                 DGEMMAMD(NULL, d_C, d_A, d_B, &ciErrNum);
             else if (__alg == 2)
                 DGEMMNVIDIA(NULL, d_C, d_A, d_B, &ciErrNum);
-            else if (((__alg >= 30) && (__alg <= 33)) || (__alg == 40) || (__alg == 41))
+            else if (((__alg >= 30) && (__alg <= 34)) || (__alg == 40) || (__alg == 41))
                 DGEMMNVIDIAPrivate(NULL, d_C, d_A, d_B, &ciErrNum);
             else if ((__alg >= 50) && (__alg <= 54))
                 DGEMMNVIDIAGlobal(NULL, d_C, d_A, d_B, &ciErrNum);
@@ -268,7 +270,7 @@ int runDGEMM(const char* program_file){
                 DGEMMAMD(NULL, d_C, d_A, d_B, &ciErrNum);
             else if (__alg == 2)
                 DGEMMNVIDIA(NULL, d_C, d_A, d_B, &ciErrNum);
-            else if (((__alg >= 30) && (__alg <= 33)) || (__alg == 40) || (__alg == 41))
+            else if (((__alg >= 30) && (__alg <= 34)) || (__alg == 40) || (__alg == 41))
                 DGEMMNVIDIAPrivate(NULL, d_C, d_A, d_B, &ciErrNum);
             else if ((__alg >= 50) && (__alg <= 54))
                 DGEMMNVIDIAGlobal(NULL, d_C, d_A, d_B, &ciErrNum);
@@ -335,7 +337,7 @@ int runDGEMM(const char* program_file){
                 closeDGEMMAMD();
             else if (__alg == 2)
                 closeDGEMMNVIDIA();
-            else if (((__alg >= 30) && (__alg <= 33)) || (__alg == 40) || (__alg == 41))
+            else if (((__alg >= 30) && (__alg <= 34)) || (__alg == 40) || (__alg == 41))
                 closeDGEMMNVIDIAPrivate();
             else if ((__alg >= 50) && (__alg <= 54))
                 closeDGEMMNVIDIAGlobal();
