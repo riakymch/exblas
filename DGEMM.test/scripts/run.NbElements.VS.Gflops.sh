54a45c52e08a78b54d8c8b63fbf94004295a8c67
riakymch <roman.iakymchuk@sorbonne-universite.fr>
2014-09-14 22:39:57 +0200

    Modified the script

--- DGEMM.test/scripts/run.NbElements.VS.Gflops.sh
+++ DGEMM.test/scripts/run.NbElements.VS.Gflops.sh
@@ -11,13 +11,35 @@ fi
 begin=256
 step=256
 nMax=2048
-exe=./../src/main.out.nvidia
-file=../results/NbElements.VS.Gflops.K20c.2014.09.11.Round.dat
+exe=./../src/main.out.amd
+file=../results/NbElements.VS.Gflops.Fusion.2014.09.14.Round.dat
 
 touch $file
 echo -n "" > $file
 
 for alg in 0 2 
+do
+    for ((n=${begin}; n<=${nMax}; n+=${step}))
+    do
+        $exe -m $n -n $n -k $n -r 1 -e 0 -a $alg | tee -a $file
+    done
+done
+
+#for alg in 54 34
+#do
+#    for ((range=3; range<=8; range+=i))
+#    do
+#     	for ((ml=2; ml<=8; range+=1))
+#	do
+#	    for ((n=${begin}; n<=${nMax}; n+=${step}))
+#            do
+#                $exe -m $n -n $n -k $n -r 1 -e $range -a $alg -ml $ml | tee -a $file
+#            done
+#	done
+#    done
+#done
+
+for alg in 30 32 33 50 52 53
 do
     for ((n=${begin}; n<=${nMax}; n+=${step}))
     do
@@ -36,25 +58,3 @@ do
         done
     done
 done
-#for alg in 54 34
-#do
-#    for ((range=3; range<=8; range+=i))
-#    do
-#     	for ((ml=2; ml<=8; range+=1))
-#	do
-#	    for ((n=${begin}; n<=${nMax}; n+=${step}))
-#            do
-#                $exe -m $n -n $n -k $n -r 1 -e $range -a $alg -ml $ml | tee -a $file
-#            done
-#	done
-#    done
-#done
-
-for alg in 50 52 53 30 32 33
-do
-    for ((n=${begin}; n<=${nMax}; n+=${step}))
-    do
-        $exe -m $n -n $n -k $n -r 1 -e 0 -a $alg | tee -a $file
-    done
-done
-
