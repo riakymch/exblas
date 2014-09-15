d54e860b844624576a3ef0ccad28e694cc328c0e
riakymch <roman.iakymchuk@sorbonne-universite.fr>
2014-09-15 15:36:51 +0200

    Added a complete set of results on k20c

--- DGEMM.test/scripts/run.NbElements.VS.Gflops.sh
+++ DGEMM.test/scripts/run.NbElements.VS.Gflops.sh
@@ -11,19 +11,19 @@ fi
 begin=256
 step=256
 nMax=2048
-exe=./../src/main.out.amd
-file=../results/NbElements.VS.Gflops.Fusion.2014.09.14.Round.dat
+exe=./../src/main.out.nvidia
+file=../results/NbElements.VS.Gflops.K20c.2014.09.14.Round.1.dat
 
 touch $file
 echo -n "" > $file
 
-for alg in 0 2 
-do
-    for ((n=${begin}; n<=${nMax}; n+=${step}))
-    do
-        $exe -m $n -n $n -k $n -r 1 -e 0 -a $alg | tee -a $file
-    done
-done
+#for alg in 0 2 
+#do
+#    for ((n=${begin}; n<=${nMax}; n+=${step}))
+#    do
+#        $exe -m $n -n $n -k $n -r 1 -e 0 -a $alg | tee -a $file
+#    done
+#done
 
 #for alg in 54 34
 #do
@@ -39,13 +39,13 @@ done
 #    done
 #done
 
-for alg in 30 32 33 50 52 53
-do
-    for ((n=${begin}; n<=${nMax}; n+=${step}))
-    do
-        $exe -m $n -n $n -k $n -r 1 -e 0 -a $alg | tee -a $file
-    done
-done
+#for alg in 30 32 33 50 52 53
+#do
+#    for ((n=${begin}; n<=${nMax}; n+=${step}))
+#    do
+#        $exe -m $n -n $n -k $n -r 1 -e 0 -a $alg | tee -a $file
+#    done
+#done
 
 #DGEMM with various FPEs
 for alg in 51 31
