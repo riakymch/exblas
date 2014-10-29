390a72f4c0dedc68f1fef26f7401b9c39d2189b5
riakymch <roman.iakymchuk@sorbonne-universite.fr>
2014-10-29 11:44:05 +0100

    Fixed bugs and added new mechanism

--- TRSV/matlab/trsv_sym.m
+++ TRSV/matlab/trsv_sym.m
@@ -2,20 +2,18 @@ function trsv_sym()
   condA = [];
   err_d = [];
   err_k = [];
+  
+  for alpha = 30:5:100
+%     A = triu(rand(i));
+%     alpha = i * i;
+%     for j=1:i
+%         A(j,j) = 1 - (1 - alpha^(-1)) * (j - 1) / (i - 1);
+%     end
+%     b = rand(i, 1);
 
-  for i = 10:10:20
-    A = triu(rand(i));
-    alpha = i * i;
-    for j=1:i
-        A(j,j) = 1 - (1 - alpha^(-1)) * (j - 1) / (i - 1);
-        %A(j,j) = 1.0;
-    end
-    %A(i/2,i/2) = alpha^(-1);
-
-    b = rand(i, 1);
-
-    [condA(i/10), err_d(i/10), err_k(i/10)] = trsv_unn_exact(i, A, b);
-    i
+    [A, b] = trsv_gen_unn(alpha, 10);
+    [condA(alpha/5), err_d(alpha/5), err_k(alpha/5)] = trsv_unn_exact(10, A, b);
+    alpha
   end
 
   err_d
@@ -25,9 +23,38 @@ function trsv_sym()
   %ax = plotyy(condA, err_d, condA, err_k);
   plot(condA, err_d, condA, err_k);
   xlabel('CondA');
-  ylabel('Error Double');
+  ylabel('Error');
   legend('err_d','err_k')
   %ylabel(ax(2), 'Error Kulisch');
+end
+
+function [A, b] = trsv_gen_unn(alpha, n)
+    A = zeros(n,n);
+    
+    for i = 1:n
+        A(i,i) = 1;
+    end
+    for i = n-1:-2:1
+        A(i,n) = 1;
+    end    
+    for i = n-2:-2:1
+        for j = i+1:n
+            A(i,j) = (-1)^(i+j+1) * 2^alpha;
+        end
+    end
+    for i = n-3:-2:1
+        for j = i+1:n-1
+            A(i,j) = (-1)^(i+j);
+        end
+    end
+    
+    b = zeros(n, 1);
+    for i = n:-2:1
+        b(i) = 1;
+    end
+    for i = n-1:-2:1
+        b(i) = 2;
+    end    
 end
 
 function x = trsv_unn_d(n, A, b)
@@ -51,7 +78,7 @@ function x = trsv_unn_kulisch(n, A, b)
   for i = n:-1:1
     s = sym(b(i));
     for j = i+1:n
-      s = sym(s - sym(A(i,j)) * sym(x(j)));
+      s = sym(s - A(i,j) * x(j));
     end
     x(i) = double(s) / A(i, i);
   end
@@ -59,15 +86,15 @@ end
 
 function [condA, err_d, err_k] = trsv_unn_exact(n, A, b)
   %double
-  x_k = sym(trsv_unn_kulisch(n, A, b));
-  x_d = sym(trsv_unn_d(n, A, b));
+  x_k = trsv_unn_kulisch(n, A, b);
+  x_d = trsv_unn_d(n, A, b);
 
   A = sym(A);
   b = sym(b);
   x_e = sym(zeros(n, 1));
 
   x_e = A \ b;
-  %max(double(abs(A * x_e - b)))
+  
 
   %compute error
   norm_x_e = max(double(abs(x_e)));
@@ -76,6 +103,7 @@ function [condA, err_d, err_k] = trsv_unn_exact(n, A, b)
 
   %compute cond number
   A_inv = inv(A);
+  %condA = cond(double(A), Inf);
   condA = norminf_m(A, n) * norminf_m(A_inv, n);
 end
 
@@ -84,10 +112,8 @@ function res = norminf_m(A, n)
 
   for i = 1:n
       sum = sym(0.0);
-      for j = 1:n
-          if i >= j
-            sum = sum + abs(A(i,j));
-          end
+      for j = i:n
+          sum = sum + abs(A(i,j));
       end
       sum_d = double(abs(sum));
       if res < sum_d
