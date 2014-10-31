84a8ea254e069b923c04f13d93cc3183c4022917
riakymch <roman.iakymchuk@sorbonne-universite.fr>
2014-10-31 12:25:37 +0100

    Added Nicolas example

--- TRSV/matlab/trsv_unn_sym.m
+++ TRSV/matlab/trsv_unn_sym.m
@@ -3,14 +3,13 @@ function trsv_unn_sym()
   err_d = [];
   err_k = [];
   
-  j=1;
-  %unn
-  alpha=50;
-  for i = 10:10:100
-    [A, b] = trsv_gen_unn_my(i);
+  n = 20;
+  c = 20 * sort(rand(n,1))
+  for i = 1:n
+    %[A, b] = trsv_gen_unn_my(i);
+    [A, b] = trsv_gen_unn_nico(40, c(i));
     
-    [condA(j), err_d(j), err_k(j)] = trsv_unn_exact(i, A, b);
-    j = j+1;
+    [condA(i), err_d(i), err_k(i)] = trsv_unn_exact(40, A, b);
     i
   end
 
@@ -19,7 +18,7 @@ function trsv_unn_sym()
   condA
 
   %ax = plotyy(condA, err_d, condA, err_k);
-  loglog(condA, err_d, condA, err_k);
+  loglog(condA, err_d, '+', condA, err_k, 'o');
   %loglog(condA, err_k);
   %ax = plotyy(condA, err_d, condA, err_k, 'loglog');
   xlim([1, 10^50]);
@@ -29,14 +28,6 @@ function trsv_unn_sym()
   legend('err_d','err_k');
   grid on;
   %ylabel(ax(2), 'Error Kulisch');
-end
-
-function [A, b] = trsv_gen_unn_my(n)
-    A = triu(rand(n));
-    for i = 1:n
-        A(i,i) = 10^((-1)^i*10*(i-1)/n);
-    end  
-    b = rand(n, 1);
 end
 
 function x = trsv_unn_d(n, A, b)
@@ -106,3 +97,60 @@ function res = norminf_m(A, n)
       end
   end
 end
+
+function [A, b] = trsv_gen_unn_my(n)
+    A = triu(rand(n));
+    for i = 1:n
+        A(i,i) = 10^((-1)^i*10*(i-1)/n);
+    end  
+    b = rand(n, 1);
+end
+
+function [x, y] = TwoSum(a,b)
+    x = a + b;
+    z = x - a;
+    y = (a - (x - z)) + (b - z);
+end
+
+function t = VecSum(p, n)
+    s(1) = p(1);
+    for i = 2:n
+        [s(i), t(i-1)] = TwoSum(s(i-1),p(i));
+    end
+    t(n) = s(n);
+end
+
+function [A, b] = trsv_gen_unn_nico(n, c)
+    A = zeros(n);
+    b = zeros(n, 1);
+    
+    % Si n est impair, on se ramene au cas pair
+    if ~mod(n,2)
+        A(n,n) = 1.0;
+        b(n,1) = 1.0;
+        n = n-1;
+    end
+    p = (n+1)/2; % n = 2*p - 1
+    
+    %  On commence par generer A(1:p, 1:n) et b(1:p)
+    D = diag((ones(1,p) - 2*rand(1,p)) .* linspace(10^(-c),10^(c),p));
+    U = triu((1 - 2*rand(p)) .* (10.^round(c*(1 - 2*rand(p)))), 1);
+    A(1:p,1:p) = D + U;
+    % A l'aide de l'algorithme VecSum, on calcule A(1:p, p+1:n) et b(1:p)
+    % de maniere a ce que l'on aie exactement sum(A(i:)) = b(i)
+    for i = 1:p
+        t = VecSum(A(i,1:p), p);
+        A(i,p+1:2*p-1) = -t(1:p-1);
+        b(i) = t(p);
+    end
+    
+    % On genere maintenant A(p+1:n,p+1:n) et b(p+1:n)
+    % A(p+1:n,p+1:n) est generee aleatoirement avec des coefficient 
+    % compris entre -1 et 1
+    A(p+1:n,p+1:n) = triu(ones(p-1) - 2*rand(p-1));
+    % b(p+1:n) est le resultat du produit A(p+1:n,p+1:n) * ones(p-1,1)
+    % calcule avec une grande precision
+    b(p+1:n) = sym(A(p+1:n,p+1:n)) * sym(ones(p-1,1));
+    b(p+1:n) = double(b(p+1:n));
+end
+
