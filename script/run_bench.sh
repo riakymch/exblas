74de29998b33a3bd95d353d2a5055a047a90c696
riakymch <roman.iakymchuk@sorbonne-universite.fr>
2015-07-30 11:37:13 +0200

    Removed

--- script/run_bench.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:"$HOME/lib"
-export KMP_AFFINITY=compact
-export OMP_NUM_THREADS=240
-export MIC_OMP_NUM_THREADS=240
-
-for i in $(seq 16 29); do
-    micnativeloadex ./../build/tests/test.exsum -a "$i 1" > /dev/null
-done
