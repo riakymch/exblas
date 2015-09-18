5a8d284ebe9df86cf46f9f4b97ba159b4637950a
riakymch <roman.iakymchuk@sorbonne-universite.fr>
2015-09-18 13:35:04 +0200

    Merged master with branch sum.test

--- scripts/run_bench_gemm.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-nbMax=2048
-step=64
-
-for ((i=64; i<=${nbMax}; i+=${step}))
-do
-    ./../build/tests/test.exgemm $i $i $i 1 > /dev/null 
-done
--- scripts/run_bench_sum_gpu.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-for i in $(seq 16 29); do
-    ./../build/tests/test.exsum $i 1 > /dev/null
-done
